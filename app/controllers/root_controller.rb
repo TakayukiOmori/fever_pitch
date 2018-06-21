e54b80fb08bbf3f9c5f14dc661496b4a0b131d8d
tatsuki1116 <tatsuki1617@gmail.com>
2018-06-21 19:24:25 +0000

    second

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,17 @@
 class RootController < ApplicationController
 
+	def top
+	end
+
 	def company
-		
+	end
+
+	def service
+	end
+
+	def fever_us
+	end
+
+	def contact
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "info/company" => "root#company"
+  root to: 'root#top'
+  get "/company/info" => "root#company"
+  get "/service" => "root#service"
+  get "/fever_us" => "root#fever_us"
+  get "/contact" => "root#contact"
 end
